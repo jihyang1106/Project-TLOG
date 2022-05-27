cc4b7b203af1be797cd8e2475463ef861ad1f5ce
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2022-05-27 16:02:31 +0900

    AWS DB연동 완료

--- src/main/java/com/tworaveler/tlog/TlogApplication.java
+++ src/main/java/com/tworaveler/tlog/TlogApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//임시  :DB 없이 프로젝트 실행해보기 위해 추가한 어노테이션
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class TlogApplication {
 
 	public static void main(String[] args) {
