fa550fed2dab880c3d7491e6a7b1a13f962f8cc5
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2023-02-25 22:38:35 +0900

    로그인 실패, REDIRECT_URI 변경_5

---
+++ src/main/java/com/tworaveler/tlog/config/WebMvcConfig.java
@@ -0,0 +1,8 @@
+package com.tworaveler.tlog.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+}
